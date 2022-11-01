69fe057e2750bdfc260bfaec008c36cf436e383b
M. Muqiit Faturrahman <24507383+mmuqiitf@users.noreply.github.com>
2022-11-01 13:30:18 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::post('/login', [LoginFromThirdPartyController::class, 'mahasiswa']);
+Route::post('/login-mahasiswa', [LoginFromThirdPartyController::class, 'mahasiswa']);
